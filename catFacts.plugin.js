3a467beb7b7c8f7b7d6eee69df6ac927b96c4299
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-07-05 17:19:14 -0400

    Update catFacts.plugin.js

--- catFacts.plugin.js
+++ catFacts.plugin.js
@@ -2,7 +2,7 @@
  * @name catFacts
  * @version 0.0.1
  * @description Sends cat facts!
- * @updateURL https://
+ * @updateURL https://raw.githubusercontent.com/amukh1/catFacts/main/catFacts.plugin.js
  * @website https://amukh1.dev
  */
 
